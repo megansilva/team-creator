d0552637f77e007294c2b052ef989536c41210e3
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-05-22 10:48:52 -0400

    adding employee test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,2 +1,59 @@
-const employee = require ("../lib/employee");
+const Employee = require ("../lib/employee");
 
+describe ("Employee", () => {
+    it ("can start employee step", () => {
+        const e = new Employee ();
+        expect(typeof(e)).toBe("object");
+    });
+
+    it ("can set the name for the constructed arguments", () => {
+        const name = "Megan";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    });
+
+    it ("can set id for the constructed argument", () => {
+        const testInfo = 100;
+        const e = new Employee("Foo", testInfo);
+        expect(e.id).toBe(testInfo);
+    });
+
+    it ("can set the email for the constructed argument", () => {
+        const testInfo = "test@work.com";
+        const e = new Employee("Foo", 1, testInfo);
+        expect(e.email).toBe(testInfo);
+    });
+
+    describe ("getName", () => {
+        it ("can grab the name of the employee being input", () => {
+            const testInfo = "Megan";
+            const e = new Employee(testInfo);
+            expect(e.getName()).toBe(testInfo);
+        });
+    });
+
+    describe ("getID", () => {
+        it ("can grab the id of the employee being input", () => {
+            const testInfo = 100;
+            const e = new Employee("Foo", testInfo);
+            expect(e.getID()).toBe(testInfo);
+        });
+    });
+
+    describe ("getEmail", () => {
+        it ("can grab the email of the employee being input", () => {
+            const testInfo = "test@work.com";
+            const e = new Employee("Foo", 1, testInfo);
+            expect(e.getEmail()).toBe(testInfo);
+        });
+    });
+
+    describe ("getRole", () => {
+        it ("getRole should return Employee", () => {
+            const testInfo = "Employee";
+            const e = new Employee ("Megan", 1, "test@work.com");
+            expect(e.getRole()).toBe(testInfo);
+        });
+    });
+
+});
